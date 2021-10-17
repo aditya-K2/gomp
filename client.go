eecb86ed8f8e8254f32f1119b9604c5e4c65d0ae
aditya-K2 <adityakurdunkar2@gmail.com>
2021-10-17 21:42:02 +0530

    Updated the Client Functions
    Before the Update Function needed the currentDirectoryMap ( the
    Functionality was not confirmed ) as a parameter but now I have removed
    the currentDirectoryMap. Instead I have added totalPath ( going to
    rename this to `absolutePath` )  to the FileNode Struct which is added
    during the generation of the directory Tree whenever the selected field
    is called we can just pass the totalPath.

--- client.go
+++ client.go
@@ -7,56 +7,59 @@ import (
 	// "fmt"
 )
 
+var directoryMap map[string][]int = make(map[string][]int)
+
 func togglePlayBack(connection mpd.Client) error {
 	status, err := connection.Status()
-	if(status["state"] == "play" && err == nil){
+	if status["state"] == "play" && err == nil {
 		connection.Pause(true)
-	} else if(status["state"] == "pause" && err == nil) {
+	} else if status["state"] == "pause" && err == nil {
 		connection.Play(-1)
 	}
 	return err
 }
 
-func join ( stringSlice [] string ) string{
+func join(stringSlice []string) string {
 	var _s string = stringSlice[0]
-	for i:= 1; i<len(stringSlice);i++{
-		if ( _s != ""){
-			_s += ( "/" + stringSlice[i])
+	for i := 1; i < len(stringSlice); i++ {
+		if _s != "" {
+			_s += ("/" + stringSlice[i])
 		}
 	}
 	return _s
 }
 
-func Update(conn mpd.Client, f []FileNode, currentDirectoryStructure [] string, inputTable *tview.Table) {
-	ab := join(currentDirectoryStructure)
-	for i,j := range(f){
+func Update(conn mpd.Client, f []FileNode, inputTable *tview.Table) {
+	inputTable.Clear()
+	for i, j := range f {
 		if len(j.children) == 0 {
-			a, err := conn.ListAllInfo(ab)
-			if err == nil {
-
+			_songAttributes, err := conn.ListAllInfo(j.totalPath)
+			if err == nil && _songAttributes[0]["Title"] != "" {
 				inputTable.SetCell(i, 0,
-				tview.NewTableCell("[#fbff00]" + a[0]["Title"]).
-				SetAlign(tview.AlignLeft))
+					tview.NewTableCell("[#fbff00]"+_songAttributes[0]["Title"]).
+						SetAlign(tview.AlignLeft))
 
 				inputTable.SetCell(i, 1,
-				tview.NewTableCell("[#000000]" + a[0]["Artist"]).
-				SetAlign(tview.AlignLeft))
+					tview.NewTableCell("[#fbff00]"+_songAttributes[0]["Artist"]).
+						SetAlign(tview.AlignLeft))
 
 				inputTable.SetCell(i, 2,
-				tview.NewTableCell("[#ff0030]" + a[0]["Album"]).
-				SetAlign(tview.AlignLeft))
+					tview.NewTableCell("[#ff0030]"+_songAttributes[0]["Album"]).
+						SetAlign(tview.AlignLeft))
 
+			} else if _songAttributes[0]["Title"] == "" {
+				inputTable.SetCell(i, 0,
+					tview.NewTableCell("[blue]"+j.path).
+						SetAlign(tview.AlignLeft))
 			}
 		} else {
-
 			inputTable.SetCell(i, 0,
-			tview.NewTableCell("[#fbff00::b]" + j.path).
-			SetAlign(tview.AlignLeft))
-
+				tview.NewTableCell("[#fbff00::b]"+j.path).
+					SetAlign(tview.AlignLeft))
 		}
 	}
 }
 
-func addSong(conn mpd.Client, currentDirectoryStructure [] string, currentCellContent string){
+func addSong(conn mpd.Client, currentDirectoryStructure []string, currentCellContent string) {
 	conn.Add(join(currentDirectoryStructure) + "/" + currentCellContent)
 }
