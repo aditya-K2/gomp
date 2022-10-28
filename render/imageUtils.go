3099b1b989e31b98523ed7fcba9980ca7b00cd70
aditya-K2 <adityakurdunkar2@gmail.com>
2022-10-28 13:28:49 +0530

    moved image related utilities to imageUtils.go

--- render/imageUtils.go
+++ render/imageUtils.go
@@ -2,9 +2,18 @@ package render
 
 import (
 	"errors"
+	"fmt"
+	"image"
 	"os"
 
+	"github.com/aditya-K2/gomp/cache"
+	"github.com/aditya-K2/gomp/client"
+	"github.com/aditya-K2/gomp/notify"
+	"github.com/aditya-K2/gomp/ui"
+	"github.com/aditya-K2/gomp/utils"
 	"github.com/dhowden/tag"
+	"github.com/nfnt/resize"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -36,4 +45,58 @@ func ExtractImage(path string, imagePath string) (string, error) {
 	}
 
 	return imagePath, nil
+}
+
+// GetImagePath This Function returns the path to the image that is to be
+// rendered it checks first for the image in the cache
+// else it adds the image to the cache and then extracts it and renders it.
+func GetImagePath(path string) string {
+	a, err := client.Conn.ListInfo(path)
+	var extractedImage string = viper.GetString("DEFAULT_IMAGE_PATH")
+	if err == nil && len(a) != 0 {
+		if cache.Exists(a[0]["artist"], a[0]["album"]) {
+			extractedImage = cache.GenerateName(a[0]["artist"], a[0]["album"])
+		} else {
+			imagePath := cache.GenerateName(a[0]["artist"], a[0]["album"])
+			absPath := utils.CheckDirectoryFmt(viper.GetString("MUSIC_DIRECTORY")) + path
+			if _eimg, exErr := ExtractImage(absPath, imagePath); exErr != nil {
+				if viper.GetString("GET_COVER_ART_FROM_LAST_FM") == "TRUE" {
+					downloadedImage, lFmErr := getImageFromLastFM(a[0]["artist"], a[0]["album"], imagePath)
+					if lFmErr == nil {
+						notify.Notify.Send("Image From LastFM")
+						extractedImage = downloadedImage
+					} else {
+						notify.Notify.Send(exErr.Error())
+					}
+				}
+			} else {
+				notify.Notify.Send("Image Extracted Succesfully!")
+				extractedImage = _eimg
+			}
+		}
+	} else {
+		notify.Notify.Send(fmt.Sprintf("Couldn't Get Attributes for %s", path))
+	}
+	return extractedImage
+}
+
+// Gets the Image Struct from the provided path
+func GetImg(uri string) (image.Image, error) {
+	f, err := os.Open(uri)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	fw, fh := utils.GetFontWidth()
+	img = resize.Resize(
+		uint(float32(ui.ImgW)*(fw+float32(viper.GetFloat64("IMAGE_WIDTH_EXTRA_X")))),
+		uint(float32(ui.ImgH)*(fh+float32(viper.GetFloat64("IMAGE_WIDTH_EXTRA_Y")))),
+		img,
+		resize.Bilinear,
+	)
+	return img, nil
 }
--- render/render.go
+++ render/render.go
@@ -1,18 +1,11 @@
 package render
 
 import (
-	"fmt"
-	"image"
-	"os"
-
-	"github.com/aditya-K2/gomp/client"
 	"github.com/aditya-K2/gomp/notify"
 	"github.com/aditya-K2/gomp/ui"
 	"github.com/fhs/gompd/v2/mpd"
 
-	"github.com/aditya-K2/gomp/cache"
 	"github.com/aditya-K2/gomp/utils"
-	"github.com/nfnt/resize"
 	"github.com/spf13/viper"
 	"gitlab.com/diamondburned/ueberzug-go"
 )
@@ -73,59 +66,6 @@ func OpenImage(path string, c chan string) {
 	} else {
 		OpenImage("stop", c)
 	}
-}
-
-// GetImagePath This Function returns the path to the image that is to be
-// rendered it checks first for the image in the cache
-// else it adds the image to the cache and then extracts it and renders it.
-func GetImagePath(path string) string {
-	a, err := client.Conn.ListInfo(path)
-	var extractedImage string = viper.GetString("DEFAULT_IMAGE_PATH")
-	if err == nil && len(a) != 0 {
-		if cache.Exists(a[0]["artist"], a[0]["album"]) {
-			extractedImage = cache.GenerateName(a[0]["artist"], a[0]["album"])
-		} else {
-			imagePath := cache.GenerateName(a[0]["artist"], a[0]["album"])
-			absPath := utils.CheckDirectoryFmt(viper.GetString("MUSIC_DIRECTORY")) + path
-			if _eimg, exErr := ExtractImage(absPath, imagePath); exErr != nil {
-				if viper.GetString("GET_COVER_ART_FROM_LAST_FM") == "TRUE" {
-					downloadedImage, lFmErr := getImageFromLastFM(a[0]["artist"], a[0]["album"], imagePath)
-					if lFmErr == nil {
-						notify.Notify.Send("Image From LastFM")
-						extractedImage = downloadedImage
-					} else {
-						notify.Notify.Send(exErr.Error())
-					}
-				}
-			} else {
-				notify.Notify.Send("Image Extracted Succesfully!")
-				extractedImage = _eimg
-			}
-		}
-	} else {
-		notify.Notify.Send(fmt.Sprintf("Couldn't Get Attributes for %s", path))
-	}
-	return extractedImage
-}
-
-func GetImg(uri string) (image.Image, error) {
-	f, err := os.Open(uri)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	img, _, err := image.Decode(f)
-	if err != nil {
-		return nil, err
-	}
-	fw, fh := utils.GetFontWidth()
-	img = resize.Resize(
-		uint(float32(ui.ImgW)*(fw+float32(viper.GetFloat64("IMAGE_WIDTH_EXTRA_X")))),
-		uint(float32(ui.ImgH)*(fh+float32(viper.GetFloat64("IMAGE_WIDTH_EXTRA_Y")))),
-		img,
-		resize.Bilinear,
-	)
-	return img, nil
 }
 
 func DrawCover(c mpd.Attrs, start bool) {
